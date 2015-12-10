08c4395e628ad045f95a28c7a6f9d093da11f721
tim <tim.pauls.hh@gmail.com>
2015-12-11 00:15:51 +0100

    Changed sample data SID to 1 to match given p and q values. Implemented test cases for steps 0 to 3

--- src/test/java/de/fhwedel/coinflipping/handling/ClientProtocolHandlerTest.java
+++ src/test/java/de/fhwedel/coinflipping/handling/ClientProtocolHandlerTest.java
@@ -1,5 +1,6 @@
 package de.fhwedel.coinflipping.handling;
 
+import de.fhwedel.coinflipping.config.ClientConfig;
 import de.fhwedel.coinflipping.model.Payload;
 import de.fhwedel.coinflipping.model.Protocol;
 import de.fhwedel.coinflipping.model.SampleProtocolSteps;
@@ -13,15 +14,35 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class ClientProtocolHandlerTest {
     @Test
+    public void testInitiation() throws Exception {
+        Protocol protocol = ClientProtocolHandler.initiateProtocol();
+
+        assertThat(protocol.getProtocolId()).isEqualTo(0);
+        assertThat(protocol.getStatusId()).isEqualTo(Protocol.STATUS_ID_OK);
+        assertThat(protocol.getProtocolNegotiation()).isNotNull();
+        assertThat(protocol.getProtocolNegotiation().getAvailableVersions().get(0).getVersions().size()).isEqualTo(ClientConfig.SUPPORTED_PROTOCOL_VERSIONS.length);
+    }
+
+    @Test
+    public void testHandleStep1() throws Exception {
+        Protocol protocolStep1 = JsonUtil.fromJson(SampleProtocolSteps.STEP_1, Protocol.class);
+        Protocol response = ClientProtocolHandler.handleProtocolStep(protocolStep1);
+
+        assertThat(response.getProtocolId()).isEqualTo(2);
+        assertThat(response.getKeyNegotiation()).isNotNull();
+        assertThat(response.getKeyNegotiation().getAvailableSids().get(0).getSids().size()).isEqualTo(ClientConfig.SUPPORTED_SIDS.length);
+    }
+
+    @Test
     public void testHandleStep3() throws Exception {
         Protocol protocolStep3 = JsonUtil.fromJson(SampleProtocolSteps.STEP_3, Protocol.class);
         Protocol response = ClientProtocolHandler.handleProtocolStep(protocolStep3);
 
+        assertThat(response.getProtocolId()).isEqualTo(4);
         Payload payload = response.getPayload();
         assertThat(payload).isNotNull();
-        assertThat(payload.getInitialCoin().size()).isEqualTo(2);
-        assertThat(payload.getEncryptedCoin().size()).isEqualTo(2);
+        assertThat(payload.getInitialCoin().size()).isEqualTo(ClientConfig.INITIAL_COINS.length);
+        assertThat(payload.getEncryptedCoin().size()).isEqualTo(ClientConfig.INITIAL_COINS.length);
     }
-
 
 }
