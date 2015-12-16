1c66f700a80ff590b27dd6a4fce0a80288ba8d3d
tim <tim.pauls.hh@gmail.com>
2015-12-16 21:32:19 +0100

    Make server loop to accept multiple clients in succession

--- src/main/java/de/fhwedel/coinflipping/network/CoinFlippingServer.java
+++ src/main/java/de/fhwedel/coinflipping/network/CoinFlippingServer.java
@@ -22,21 +22,23 @@ public class CoinFlippingServer extends  Transmitter {
     public static void main(String[] args) throws IOException {
         Log.info("Starting server on port " + SERVER_PORT);
 
-        try (
-            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-            mServerSocket = serverSocket;
-            mOut = out;
-            mIn = in;
+        while (true) {
+            try (
+                ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            ) {
+                mServerSocket = serverSocket;
+                mOut = out;
+                mIn = in;
 
-            Log.info("Accepted connection from: " + clientSocket.getInetAddress());
+                Log.info("Accepted connection from: " + clientSocket.getInetAddress());
 
-            performProtocol();
-        } catch (IOException e) {
-            Log.error("Exception caught when trying to listen on port " + SERVER_PORT + " or listening for a connection", e);
+                performProtocol();
+            } catch (IOException e) {
+                Log.error("Exception caught when trying to listen on port " + SERVER_PORT + " or listening for a connection", e);
+            }
         }
     }
 
